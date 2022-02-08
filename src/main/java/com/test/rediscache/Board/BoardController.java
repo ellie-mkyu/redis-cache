00cabf1b08087900099ab5a86d4251e95e9d8ea0
ellie <ellie@mkcreative.co.kr>
2022-02-08 17:59:51 +0900

    feat : board 도메인 코드 작성

---
+++ src/main/java/com/test/rediscache/Board/BoardController.java
@@ -0,0 +1,26 @@
+package com.test.rediscache.Board;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BoardController {
+
+    private final BoardService service;
+
+    public BoardController (BoardService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public List<Board> boards(String size){
+        return service.getBoards(size);
+    }
+
+    @GetMapping("/count")
+    public int count() {
+        return BoardService.getDbCount();
+    }
+}
